2c4d521340560c3cfa6565008868eaab927ff9c2
kiatklai <likhitthaworn.k@gmail.com>
2023-08-13 22:05:38 +0900

    component作成

---
+++ src/components/Dropdown.js
@@ -0,0 +1,16 @@
+const Dropdown=()=>{
+  return(
+    <nav>
+      <h2>Dynamic Dropdown</h2>
+      <select className="menu">
+        <option value="全て">全て</option>
+        <option value="炒め・揚げ物">炒め・揚げ物</option>
+        <option value="スープ・トムヤム">スープ・トムヤム</option>
+        <option value="飲料">飲料</option>
+        <option value="ステーキ">ステーキ</option>
+      </select>
+    </nav>
+  )
+}
+
+export default Dropdown
