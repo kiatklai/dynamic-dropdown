68b69992225395b97a56629f0e78d0598098529c
kiatklai <likhitthaworn.k@gmail.com>
2023-08-14 20:23:07 +0900

    内容を表示できるように

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Dropdown from "./components/Dropdown";
 import Food from "./components/Food";
+import {useState} from 'react'
+import MenuData from './data/MenuData';
 
 function App() {
+  const [foods,setFoods] = useState(MenuData)
   return (
     <div className="container">
       <Dropdown/>
-      <Food/>
+      <div className='content'>
+        {foods.map((data,index)=>{
+          return <Food key={index} {...data}/>
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Food.js
+++ src/components/Food.js
@@ -1,7 +1,8 @@
-const Food=()=>{
+const Food=({foodName,image_url})=>{
   return(
-    <div>
-      <h1>Food Component</h1>
+    <div className="single-food">
+      <h1>{foodName}</h1>
+      <img src={image_url} alt={foodName}/>
     </div>
   )
 }
